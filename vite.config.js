5d9740f231176a91c85f9baae02ff067dddb0fc5
Luciano Medina <lmedina@alteracorp.cl>
2023-04-12 16:46:41 -0400

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://lmedina2.github.io/',
+  base: 'https://lmedina2.github.io/gh-react-vite',
 
 })
